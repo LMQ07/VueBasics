96a7d9b5e30ac2b784da2ce39884c58d98051c71
summer-MQ <mq1009416078@163.com>
2022-07-13 09:47:09 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+// webpack 打包入口文件
+import Vue from "vue"; // 引入vue
+import App from "./App.vue"; // 引入根组件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false; // 提示语
 
+// 生成一个实例对象
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App), //render渲染app createElement创建元素
+}).$mount("#app"); //挂载到index的文件中
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    open: true, //自动打开浏览器
+    host: "localhost",
+  },
+  lintOnSave: false, //关闭检查
+});
