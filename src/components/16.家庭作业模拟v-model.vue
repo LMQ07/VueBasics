3199c9d366ca0e67c8933c6b045f38bb96f2a853
summer-MQ <mq1009416078@163.com>
2022-07-14 08:01:11 +0800

    第一天作业已完成

---
+++ src/components/16.家庭作业模拟v-model.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" :value="ipt" @input="handleIpt" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ipt: "123",
+    }
+  },
+  methods: {
+    handleIpt(e) {
+      this.ipt = e.target.value
+    },
+  },
+}
+</script>
