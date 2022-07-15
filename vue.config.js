73f14dd2c65cac301c7273bdea1906535e9befbd
summer-MQ <mq1009416078@163.com>
2022-07-15 08:32:21 +0800

    第二天开始了

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     open: true, //自动打开浏览器
     host: "localhost",
   },
-  lintOnSave: false, //关闭检查
-});
+  lintOnSave: true, //关闭检查
+})
