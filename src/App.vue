db830011fcde38fa6526b1d9f2609af2464906f2
summer-MQ <mq1009416078@163.com>
2022-07-15 08:25:47 +0800

    heih

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,20 @@
-
 <template>
-  <div class="container">
-    <div class="imglist">
-      <h3>图片列表区</h3>
-      <img src="./assets/01.jpg" alt="" @mouseover="getUrl" @click="setBgc">
-      <img src="./assets/02.jpg" alt="" @mouseover="getUrl" @click="setBgc">
-      <img src="./assets/03.jpg" alt="" @mouseover="getUrl" @click="setBgc">
-    </div>
-    <div class="show">
-      <h3>图片预览区</h3>
-      <img :src="imgUrl" alt="" style="width:600px;height:auto;">
-    </div>
-    <div>
-    </div>
+  <div>
+    <input type="text" :value="ipt" @input="handleIpt" />
   </div>
 </template>
 
 <script>
-import img from "./assets/01.jpg"
 export default {
   data() {
     return {
-      imgUrl: img
+      ipt: "123",
     }
   },
   methods: {
-    getUrl(e) {
-      // 这边的src路径已经经过处理了 所以才可以的
-      console.log(e.target.src);
-      this.imgUrl = e.target.src
+    handleIpt(e) {
+      this.ipt = e.target.value
     },
-    setBgc(e) {
-      document.body.style.background = `url(${e.target.src}) no-repeat 0 0/cover`
-    }
   },
-};
+}
 </script>
-
-<style scoped>
-.container {
-  display: flex;
-}
-
-.imglist {
-  border: 2px solid red;
-}
-
-.imglist img {
-  width: 200px;
-  height: auto;
-}
-
-.show {
-  border: 2px solid red;
-  margin-right: 20px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // webpack 打包入口文件
-import Vue from "vue"; // 引入vue
-import App from "./App.vue"; // 引入根组件
+import Vue from "vue" // 引入vue
+import App from "./App.vue" // 引入根组件
 
-Vue.config.productionTip = false; // 提示语
+Vue.config.productionTip = false // 提示语
 
 // 生成一个实例对象
 new Vue({
   render: (h) => h(App), //render渲染app createElement创建元素
-}).$mount("#app"); //挂载到index的文件中
+}).$mount("#app") //挂载到index的文件中
