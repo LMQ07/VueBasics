9c859a71a9475da002c9704b7dd6204126892785
summer-MQ <mq1009416078@163.com>
2022-07-17 15:06:58 +0800

    第三天作业

---
+++ src/components/MyTrs.vue
@@ -0,0 +1,38 @@
+<template>
+  <tr>
+    <th>
+      <input type="checkbox" v-model="item.checked" @change="changeCheck" />
+    </th>
+    <th>{{ item.name }}</th>
+    <th>{{ item.price }}</th>
+    <th>
+      <button @click="reduce(index)" :disabled="item.num == 0">-</button
+      >{{ item.num }}
+      <button @click="add(index)">+</button>
+    </th>
+    <th>{{ item.price * item.num }}</th>
+    <th><button @click="del(index)">删除</button></th>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ["item", "index"],
+  methods: {
+    changeCheck() {
+      this.$emit("checkAll", this.item, this.index);
+    },
+    add(index) {
+      this.$emit("addNum", index);
+    },
+    reduce(index) {
+      this.$emit("reduceNum", index);
+    },
+    del(index) {
+      this.$emit("deleteList", index);
+    },
+  },
+};
+</script>
+
+<style></style>
