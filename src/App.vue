3d25c3d9d1ab7ad8c7637ee45d01245e6cd9e713
summer-MQ <mq1009416078@163.com>
2022-07-17 08:37:19 +0800

    第三天作业

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <el-button :arr="arr"></el-button>
+    <!-- <el-button :arr="arr"></el-button> -->
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
@@ -11,9 +17,12 @@ export default {
     "el-button": MyButton,
   },
   data() {
-    return {
-      arr: ["primary", "main", "success", "information", "warning", "danger"],
-    };
+    return {};
+  },
+  methods: {
+    msg() {
+      console.log(234);
+    },
   },
 };
 </script>
